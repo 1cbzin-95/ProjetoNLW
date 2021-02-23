e7805d445cb2d022c9b33c91625fd35f79fc4e9e
“1cbzin-95” <clodoaldo.brtp4@gmail.com>
2021-02-23 20:49:30 -0300

    migrando para o next

---
+++ frontend-next/src/components/ExperienceBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ExperienceBar(){
+    return (
+        <header className="experience-bar">
+            <span>0 xp</span>
+            <div>
+                <div style={{width:'60%'}} />
+            </div>
+            <span>600 xp</span>
+        </header>
+    );
+}
+export default ExperienceBar;
